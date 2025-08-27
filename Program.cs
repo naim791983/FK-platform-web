a652c820550be079ddc3a979b85924aa35a8bd04
Rabie Benboubaker <rabie.benboubaker@pmelcom.com>
2025-08-27 12:11:38 +0200

    Publish Blazor WASM to GitHub Pages

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using FK_platform_web;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
